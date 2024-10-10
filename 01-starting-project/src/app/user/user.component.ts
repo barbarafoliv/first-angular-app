32eb626a5ca545c82def331c99d70bfa59e33c77
Barbara Oliveira <barbara.oliveira@kuehne-nagel.com>
2024-10-10 18:09:24 +0100

    Angular essentials: defining component inputs pt.2

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,18 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output, output} from '@angular/core';
+
+/*
+type User = {
+  id: string;
+  avatar: string;
+  name: string;
+};
+*/
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -7,12 +21,32 @@ import {Component, Input} from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  /*
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string; // used to avoid TS error about properties being potentially undefined
+  @Input({ required: true }) name!: string; // "required" was introduced in v16, an error is thrown if parent component fails to pass a value for that input
+  */
+  @Input({ required: true }) user!: User;
+
+  @Output() select = new EventEmitter<string>();
+  /*select = output<string>();*/
+
+  /*
+  avatar = input.required<string>(); // Signal = Data container
+  name = input.required<string>(); // These are read-only
+  */
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
-  onSelectUser() {}
+  /*
+  imagePath = computed(() => { // computed() recalculates whenever the value of "avatar" changes
+    return 'assets/users/' + this.avatar();
+  });
+  */
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
 }
